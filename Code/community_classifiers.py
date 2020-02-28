75976282c08dc52faa3103ddfbd5e7af594a4b58
Paul Champion <paul.champion92@gmail.com>
2020-02-28 17:05:31 +0100

    Edit Ncommunity classifier

--- Code/community_classifiers.py
+++ Code/community_classifiers.py
@@ -39,6 +39,7 @@ class TwoCommunityClassifier():
         # Compute the simple power method (TBC, does not converge)
 
         #Alternatively, use built in function for eigenvalues
+        # import ipdb; ipdb.set_trace()
         vals,vecs=eig(self.B)
         self.leading_eigenvector=np.ravel(vecs[:,np.argmax(vals)])
         if np.max(self.leading_eigenvector)*np.min(self.leading_eigenvector)>=0: #All elements of the same sign
@@ -53,56 +54,65 @@ class TwoCommunityClassifier():
 
 # The idea is to apply the previous algorithm recursively each community until convergence of the process.
 class NCommunityClassifier(TwoCommunityClassifier):
-    def __init__(self,graph,B=None):
+    def __init__(self,graph,B=None,category=None):
         super().__init__(graph,B)
+        if category:
+            self.category=category
         # self.Q=Tree()
     
     def Beq(self,nodes):
         #compute the equivalent matrix Beq
         # nodes_selected=self.G.nodes==nodes
         nodes_selected=nodes
-        Beq=self.B[nodes_selected,nodes_selected]
+        Beq=self.B[nodes_selected,:][:,nodes_selected]
+        # import ipdb; ipdb.set_trace()
         Beq-=np.diagonal(np.sum(Beq,axis=1))
         return Beq
 
-    def fit(self,graph=None,B=None):
-        
+    def fit(self,graph=None,B=None,category=None):
+    
         if graph is None:
-            clf=TwoCommunityClassifier(self.G,self.B)
-        else:
-            clf=TwoCommunityClassifier(graph,B)
+            # clf=TwoCommunityClassifier(self.G,self.B)
+            graph=self.G
+        if category:
+            self.category=category
+
+        clf=TwoCommunityClassifier(graph,B)
         clf.fit()
         # Push value of Q at the current node in the tree
         if clf.done:
             # If undivisible graph, terminate the classification
-            for i,node in enumerate(self.G.nodes):
+            for i,node in enumerate(graph.nodes):
+                import ipdb; ipdb.set_trace()
                 self.category[node].append(1 if clf.leading_eigenvector[i]>=0 else -1)
         else:
             #If the graph is divisible, divide it and apply the algorithm to the subgraphs.
-        
             nodes=np.arange(len(clf.leading_eigenvector))
             nodes_positive=nodes[clf.leading_eigenvector>=0]
             nodes_negative=nodes[clf.leading_eigenvector<0]
-            subgraph_positive=self.G.subgraph(nodes_positive)
-            subgraph_negative=self.G.subgraph(nodes_negative)
+            
+            subgraph_positive=graph.subgraph(nodes_positive)
+            subgraph_negative=graph.subgraph(nodes_negative)
+
             B_positive=self.Beq(nodes_positive)
             B_negative=self.Beq(nodes_negative)
-            self.fit(subgraph_positive,B_positive)
-            self.fit(subgraph_negative,B_negative)
-            #Push the DQ values where relevant
+            
+            self.fit(subgraph_positive,B_positive,category)
+            self.fit(subgraph_negative,B_negative,category)
+            # Push the DQ values where relevant
 
 
 
 # ---------------------------------- Results --------------------------------- #
 
-clf=TwoCommunityClassifier(G)
-clf.fit()
+# clf=TwoCommunityClassifier(G)
+# clf.fit()
 
-# print("Leading eigenvector")
-# print(clf.leading_eigenvector)
-print("Communauté")
-print(clf.category)
-print("Q: %f"%(clf.Q))
+# # print("Leading eigenvector")
+# # print(clf.leading_eigenvector)
+# print("Communauté")
+# print(clf.category)
+# print("Q: %f"%(clf.Q))
 
 clfN=NCommunityClassifier(G)  
 clfN.fit()  
