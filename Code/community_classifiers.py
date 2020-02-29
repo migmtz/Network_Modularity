f3b8614b5739523626cd68a015451e2cc57379a8
Paul Champion <paul.champion92@gmail.com>
2020-02-29 13:25:42 +0100

    Edit NCommunity classifier, compute the modularity curve

--- Code/community_classifiers.py
+++ Code/community_classifiers.py
@@ -2,6 +2,7 @@ import networkx
 from networkx.convert_matrix import to_numpy_matrix
 import numpy as np
 from numpy.linalg import eig
+import matplotlib.pyplot as plt
 from scipy.sparse import csc_matrix
 
 # --------------------------- Generate sample graph -------------------------- #
@@ -12,6 +13,8 @@ print("Matrice d'adjacence")
 print(to_numpy_matrix(G))
 print("Vecteurs des degrés")
 print(np.sum(to_numpy_matrix(G),axis=1))
+
+# --------------- Create tree object to store modularity values -------------- #
 
 class Tree():
     def __init__(self,root=0,left=None,right=None):
@@ -27,101 +30,82 @@ class Tree():
     
     def push_right(self,value=None):
         self.right=Tree(value)
-
-t=Tree()
-# import ipdb; ipdb.set_trace()
-    # def sum_level(self,level): 
-    # To be completed later
-    #     if level==0:
-    #         return self.root
-    #     # elif self.root
-    #     else:
-    #         if self.left is None:
-    #             return self.root+self.self.left.sum_level(level-1)+self.right.sum_level(level-1)
-            
-    #         return self..sum_level()
+    
+    def sum_level(self):
+        pass
 
 
 # ----------------------- 2-Communities classifier ---------------------- #
 
 class TwoCommunityClassifier():
     
-    def __init__(self,graph,B=None,category=None):
+    def __init__(self,graph,B=None,m=None):
         self.G=graph
         self.A=to_numpy_matrix(graph)
         self.k=np.sum(self.A,axis=1)
-        self.m=np.sum(self.k)
+        if m is None:
+            self.m=np.sum(self.k)
+        else:
+            self.m=m
         if B is None:
             self.B=self.A-np.dot(self.k,self.k.transpose())/self.m
         else:
             self.B=B
         self.leading_eigenvector=None
-        if category is None:
-            self.category={node:[] for node in self.G.nodes}
-        else:
-            self.category=category
-        # self.Q=
+        self.category={node:[] for node in self.G.nodes}
         self.done=False
 
     
     def fit(self,eps=0.5,max_iteration=1000):
-        # Compute the simple power method (TBC, does not converge)
-
-        #Alternatively, use built in function for eigenvalues
-        # import ipdb; ipdb.set_trace()
         vals,vecs=eig(self.B)
         self.leading_eigenvector=np.ravel(vecs[:,np.argmax(vals)])
         s=[1 if v>=0 else -1 for v in self.leading_eigenvector]
         for i,node in enumerate(self.G.nodes):
             self.category[node].append(s[i])
         self.Q=np.einsum("i,ij,j",s,self.B,s)/(4*self.m)
-        if self.Q>=0: #All elements of the same sign
+        if self.Q<=0 or np.max(self.leading_eigenvector)*np.min(self.leading_eigenvector)>0: #All elements of the same sign or negative modularity
             self.done=True
     
 
 # ------------------------- N-Communities classifier ------------------------- #
 
-# The idea is to apply the previous algorithm recursively each community until convergence of the process.
 class NCommunityClassifier(TwoCommunityClassifier):
-    def __init__(self,graph,B=None,category=None,Q=None):
+    def __init__(self,graph,B=None,category=None,level=None):
         super().__init__(graph,B)
         if category:
             self.category=category
-        if Q:
-            self.Q=Tree(Q)
-            self.Q.left
+        self.level=level
+        self.Q=0
+        self.N=0
     
     def Beq(self,nodes):
         #compute the equivalent matrix Beq
-
         Beq=self.B[nodes,:][:,nodes]
         Beq-=np.diagonal(np.sum(Beq,axis=1))
         return Beq
 
-    def fit(self,graph=None,B=None,category=None,Q=None,direction=None):
-    
+    def fit(self,graph=None,B=None,category=None):
+
         if graph is None:
-            # clf=TwoCommunityClassifier(self.G,self.B)
             graph=self.G
         if category:
             self.category=category
         
-        clf=TwoCommunityClassifier(graph,B,self.category)
+        clf=TwoCommunityClassifier(graph,B,self.m)
         clf.fit()
-        if Q is None:
-            self.Q=Tree(clf.Q)
-        # else:
-        #     if direction="Left":
-        #         self.Q.left=Tree(clf.Q)
-        #     else:
-        #         self.Q.right=Tree(clf.Q)
-
-        if clf.done:
-            # If undivisible graph, terminate the classification
+        if clf.done or self.level==0:
+            # If undivisible graph, do not return any classification and terminate the fitting operation 
+            return None
+        else:
+            # Otherwise, assign each node of the considered graph to its category
+            if self.level:
+                self.level-=1
+            self.Q+=clf.Q
+            self.N+=2  
             for i,node in enumerate(graph.nodes):
                 self.category[node].append(1 if clf.leading_eigenvector[i]>=0 else -1)
-        else:
-            #If the graph is divisible, divide it and apply the algorithm to the subgraphs.
+            
+            #Iterate the division on the two subgraphs
             nodes=np.arange(len(clf.leading_eigenvector))
             nodes_positive=nodes[clf.leading_eigenvector>=0]
             nodes_negative=nodes[clf.leading_eigenvector<0]
@@ -131,28 +115,48 @@ class NCommunityClassifier(TwoCommunityClassifier):
 
             B_positive=self.Beq(nodes_positive)
             B_negative=self.Beq(nodes_negative)
-            
             self.fit(subgraph_positive,B_positive,category)
             self.fit(subgraph_negative,B_negative,category)
-            # Q_positive=np.einsum("i,ij,j",s,self.B_positive,s)/(4*self.m)
-            # Q_negative=np.einsum("i,ij,j",s,self.B_negative,s)/(4*self.m)
-            # Push the DQ values where relevant
-    
-    # def modularity(self,Q):
-        # pass
-        
 
 
-# ---------------------------------- Results --------------------------------- #
+# ---------------------------------------------------------------------------- #
+#                                    Results                                   #
+# ---------------------------------------------------------------------------- #
 
-# clf=TwoCommunityClassifier(G)
-# clf.fit()
+clf=TwoCommunityClassifier(G)
+clf.fit()
+print("Two communities modularity: %f"%(clf.Q))
 
-# # print("Leading eigenvector")
-# # print(clf.leading_eigenvector)
-# print("Communauté")
-# print(clf.category)
-# print("Q: %f"%(clf.Q))
+
 
 clfN=NCommunityClassifier(G)  
 clfN.fit()  
+print("N-communities modularity:%f"%(clfN.Q))
+print("Number of communities found:%d"%(clfN.N))
+
+# --------------------------- Modularity evolution --------------------------- #
+
+def plot_Q(graph,eps=1e-3,maxQ=False):
+    """
+        Build a classifier stopping at each level N, compute the corresponding modularity
+    """
+    q1=0
+    q2=q1+2*eps
+    Q_results=[0]
+    i=1
+    while q2-q1>eps:
+       clfN=NCommunityClassifier(graph,level=i)
+       clfN.fit()
+       q1=q2
+       q2=clfN.Q 
+       Q_results.append(q2)
+       i+=1
+    plt.plot(np.arange(0,i),Q_results)
+    plt.xlabel("Division level")
+    plt.ylabel("Modularity")
+    plt.show()
+    if maxQ:
+        return q2
+
+plot_Q(G)
+        
