Correct calculation of B in GN and Spectral classifier

Code/DA.py
@@ -5,7 +5,7 @@ from numpy.linalg import eig
 import matplotlib.pyplot as plt
 from scipy.sparse import csc_matrix
 from matplotlib.pyplot import cm
-
+# from community_classifiers import plot_communities
 G=networkx.generators.karate_club_graph()
 
 class DA2communityClassifier():
@@ -25,9 +25,9 @@ class DA2communityClassifier():
         min_val = min(d.values())
         return [k for k in d if d[k] == min_val][0]
 
-    def fit(self,eps=1,maxiter=1000):
+    def fit(self,eps=1e-5,maxiter=1000):
         Q=self.Q+2*eps
-        count=0
+        self.count=0
         nodes=np.array(self.G.nodes)
         index_nodes=np.arange(0,len(self.G.nodes))
         index_nodes_random=np.random.permutation(index_nodes)
@@ -36,22 +36,25 @@ class DA2communityClassifier():
         graph_positive=self.G.subgraph(nodes[index_nodes_positive])
         graph_negative=self.G.subgraph(nodes[index_nodes_negative])
         category={node:1 if i in index_nodes_positive else -1 for i,node in enumerate(self.G)}
-        self.fitness={node:0 for node in self.G}
+        self.fitness={node:0 for node in self.G.nodes} #fitness between 1 and -1
         while True:
             # For each node
             for i,node in enumerate(self.G.nodes):
-                #Compute fitness
+                # Compute fitness
                 k=self.k[i] #degree of the node
-
                 if category[node]==1:
-                    k_com=graph_positive.degree[node] #degree of the node within the community
+                    k_com=graph_positive.degree[node] # degree of the node within the community
                 else:
-                    k_com=graph_negative.degree[node]
+                    k_com=graph_negative.degree[node] 
                 self.fitness[node]=2*k_com/k-1
-
-                #Move the node with lowest fitness to the opposite community
+                # Move the node with lowest fitness to the opposite community
+                # if np.random.binomial(1,1/len(index_nodes),1)==1:
+                #     # epsilon greedy strategy
+                #     node_min=np.random.randint(0,len(index_nodes),1)[0]
+                # else:
                 node_min=self.argmin(self.fitness)
                 arg_node_min=list(index_nodes).index(node_min)
+                print(arg_node_min)
                 if category[node_min]==1:
                     index_nodes_positive.remove(arg_node_min)
                     index_nodes_negative.append(arg_node_min)
@@ -60,22 +63,54 @@ class DA2communityClassifier():
                     index_nodes_negative.remove(arg_node_min)
                     index_nodes_positive.append(arg_node_min)
                     category[node_min]=1
-
-                #Recompute the subgraphs parameters
+                # Recompute the subgraphs parameters
                 graph_positive=self.G.subgraph(nodes[index_nodes_positive])
                 graph_negative=self.G.subgraph(nodes[index_nodes_negative])
-                    
             #Compute Q
             self.Q=Q
-            B=self.A-np.diag(self.k)
+            B=self.A-np.dot(self.k,self.k.transpose())/(2*self.m)
             s=np.array([1 if i in index_nodes_positive else -1 for i in range(len(self.G.nodes))])
             Q=np.einsum("i,ij,j",s,B,s)/(4*self.m)
-            count+=1
-            if abs(Q-self.Q)<eps or count>maxiter:
+            self.count+=1
+            import ipdb; ipdb.set_trace()
+            if abs(Q-self.Q)<eps or self.count>maxiter:
                 break
         #Append final result
         for node in self.G.nodes:
             self.category[node].append(category[node])
+
+def plot_communities(G,clf):
+    # Labelize lists
+    dict_aux = {}
+    dict_labels = {}
+    i = -1
+    for key,val in clf.category.items():
+        if dict_aux.get(tuple(val)) is None:
+            i += 1
+        a = dict_aux.setdefault(tuple(val),i)
+        dict_labels.setdefault(key,a)
+    print(dict_aux)
+    # Plot parameters
+    pos = networkx.kamada_kawai_layout(G)
+    rainbow = cm.rainbow(np.linspace(0,1,len(dict_aux)))
+    
+    plt.figure()
+    for k in range(len(dict_aux)):
+        nodes = [i for i in dict_labels.keys() if dict_labels[i] == k]
+        networkx.draw_networkx_nodes(G,pos,
+                                nodelist = nodes,
+                                node_color =rainbow[k].reshape(1,4),
+                                node_size=200,
+                                node_shape = 'o',
+                                label = str(k),
+                                alpha=0.8)
+
+    
+    networkx.draw_networkx_edges(G, pos, width=1.0, alpha=0.5)
+    
+    plt.legend()
+    plt.show()
+
 
 # Multiclass classifier to test
 
@@ -83,3 +118,5 @@ clf=DA2communityClassifier(G)
 clf.fit()
 print("Q-value %f"%(clf.Q))
 print("categories %s"%(str(clf.category)))
+print("count %d"%(clf.count))
+plot_communities(G,clf)

Code/GN_Spectal.py
@@ -4,6 +4,7 @@ import numpy as np
 from networkx.convert_matrix import to_numpy_matrix
 from networkx.algorithms import community
 from sklearn.cluster import SpectralClustering
+from community_classifiers import plot_communities
 
 G=nx.generators.karate_club_graph()
 
@@ -21,7 +22,7 @@ class GN2communityClassifier():
 
     def fit(self):
       partitions=next(self.communities_generator)
-      B=self.A-np.diag(self.k)
+      B=self.A-np.dot(self.k,self.k.transpose())/(2*self.m)
       s=np.array([1 if i in partitions[0] else -1 for i in range(self.G.number_of_nodes())])
       self.Q=np.einsum("i,ij,j",s,B,s)/(4*self.m)
       self.category=dict(zip(list(self.G.nodes), [[i] for i in s]))
@@ -46,7 +47,7 @@ class SPC2communityClassifier():
     def fit(self):
       sc = SpectralClustering(2, affinity='precomputed')
       sc.fit(self.A)
-      B=self.A-np.diag(self.k)
+      B=self.A-np.dot(self.k,self.k.transpose())/(2*self.m)
       s=np.array([1 if i==1 else -1 for i in sc.labels_])
       self.Q=np.einsum("i,ij,j",s,B,s)/(4*self.m)
       self.category=dict(zip(list(self.G.nodes), [[i] for i in s]))
@@ -56,3 +57,5 @@ clf_spc=SPC2communityClassifier(G)
 clf_spc.fit()
 print("Q-value for spectral clustering %f"%(clf_spc.Q))
 print("categories spectral clustering %s"%(str(clf_spc.category)))
+
+plot_communities(G,clf_spc)