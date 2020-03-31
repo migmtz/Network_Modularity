57a82a2345c0ac941c35626c1fb8407ff3712adc
Paul Champion <paul.champion92@gmail.com>
2020-03-31 10:10:32 +0200

    Finish results_generation file

--- Code/results_generation.py
+++ Code/results_generation.py
@@ -18,10 +18,58 @@ np.random.seed(1)
 #                               Graph data import                              #
 # ---------------------------------------------------------------------------- #
 
-G = networkx.read_gml('./data/polbooks.gml')
+# More examples to add
+
+G_List=[]
+G_List.append(networkx.generators.karate_club_graph())
+g_polbooks=networkx.read_gml('./data/polbooks.gml')
+g_polbooks.name="Books politics"
+G_List.append(g_polbooks)
 
 
-clfN=NCommunitiesClassifier(G,GN2communityClassifier,2)
+# ---------------------------------------------------------------------------- #
+#                                Modularity plot                               #
+# ---------------------------------------------------------------------------- #
+
+#The purpose of the section is to plot modularity as a function of the number of communities for all four algorithms
+
+# --------------------------------- Settings --------------------------------- #
+
+G=networkx.read_gml('./data/polbooks.gml') #find a larger network (does not exceed 4 communities with this one)
+n=6
+
+# ----------------------------- Plot construction ---------------------------- #
+
+clfN=NCommunitiesClassifier(G,Newman2CommunityClassifier,n)
 clfN.fit()
+plt.plot(range(1,n+1),clfN.Q_History,label="N06")
+clfN=NCommunitiesClassifier(G,DA2communityClassifier,n)
+clfN.fit()
+plt.plot(range(1,n+1),clfN.Q_History,label="DA")
+clfN=NCommunitiesClassifier(G,GN2communityClassifier,n)
+clfN.fit()
+plt.plot(range(1,n+1),clfN.Q_History,label="GN")
+clfN=NCommunitiesClassifier(G,SP2CcommunityClassifier,n)
+clfN.fit()
+plt.plot(range(1,n+1),clfN.Q_History,label="Spectral")
+plt.xlabel("Number of communities")
+plt.legend()
+plt.show()
 
-print("Modularity %.3f"%(clfN.Q))
+# ---------------------------------------------------------------------------- #
+#                           Table optimal modularity                           #
+# ---------------------------------------------------------------------------- #
+
+for G in G_List:
+    clfN=NCommunitiesClassifier(G,Newman2CommunityClassifier)
+    clfN.fit()
+    print("Optimal settings for graph %s, classifier N06: Q=%.3f, N=%d"%(G.name,clfN.Q,clfN.commmunity_count))
+    clfN=NCommunitiesClassifier(G,DA2communityClassifier)
+    clfN.fit()
+    print("Optimal settings for graph %s, classifier DA: Q=%.3f, N=%d"%(G.name,clfN.Q,clfN.commmunity_count))
+    clfN=NCommunitiesClassifier(G,GN2communityClassifier)
+    clfN.fit()
+    print("Optimal settings for graph %s, classifier GN: Q=%.3f, N=%d"%(G.name,clfN.Q,clfN.commmunity_count))
+    clfN=NCommunitiesClassifier(G,SP2CcommunityClassifier)
+    clfN.fit()
+    print("Optimal settings for graph %s, classifier Spectral: Q=%.3f, N=%d"%(G.name,clfN.Q,clfN.commmunity_count))
